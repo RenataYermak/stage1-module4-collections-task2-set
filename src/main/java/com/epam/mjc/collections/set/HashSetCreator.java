7ca1d612d996ead3b9ef813600bcff6937954c77
Renata Yermak <renatkahturo@gmail.com>
2023-04-11 18:28:13 +0300

    .

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,19 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+        for (Integer element : sourceList) {
+            if (element % 2 == 0) {
+                result.add(element);
+                while (element % 2 == 0) {
+                    element /= 2;
+                    result.add(element);
+                }
+            } else {
+                result.add(element);
+                result.add((element * 2));
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,16 @@
 package com.epam.mjc.collections.set;
 
 import java.util.List;
+import java.util.NavigableSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        NavigableSet<Integer> result = new TreeSet<>();
+        for (Integer element : sourceList) {
+            result.add(element * element);
+        }
+        return result.subSet(lowerBound, true, upperBound, true);
     }
 }
